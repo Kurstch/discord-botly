90267f3e0bb12a1fc5af62ed5cc0ed512c122b6e
Kurstch <61703543+Kurstch@users.noreply.github.com>
2022-05-29 16:32:41 +0300

    fix: Fix typings and exports #20 (#22)
    Fixes #20

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-export { init } from './init';
-export { registerGlobalSlashCommands } from './registerCommands';
+import { init } from './init';
+import { registerGlobalSlashCommands } from './registerCommands';
+
+// Export in multiple ways to allow developers to
+// import in their preferred way
+
+export {
+    init,
+    registerGlobalSlashCommands,
+};
+
+export default {
+    init,
+    registerGlobalSlashCommands,
+};
