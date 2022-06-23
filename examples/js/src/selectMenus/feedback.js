811be62b27bd353923966ac0a37d1930bda08b8c
Kurstch <61703543+Kurstch@users.noreply.github.com>
2022-06-23 19:19:49 +0300

    docs: Remove `examples` directory

--- examples/js/src/selectMenus/feedback.js
+++
@@ -1,7 +0,0 @@
-exports.execute = interaction => {
-    interaction.reply({
-        ephemeral: true,
-        content: 'Thank you for your feedback!'
-    });
-    console.log(`feedback from ${interaction.user.tag}: ${interaction.values}`);
-};
