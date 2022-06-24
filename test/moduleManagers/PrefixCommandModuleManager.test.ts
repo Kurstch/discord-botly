bf7f5866f546000705f4c95fd7118d3d3061145b
Kurstch <61703543+Kurstch@users.noreply.github.com>
2022-06-24 14:39:34 +0300

    fix: Update PrefixCommandModuleManager tests to use new data

--- test/moduleManagers/PrefixCommandModuleManager.test.ts
+++ test/moduleManagers/PrefixCommandModuleManager.test.ts
@@ -35,7 +35,7 @@ describe('Testing PrefixCommandModuleManager', () => {
         callback(message);
 
         expect(matchSpy).toHaveBeenCalledWith(message);
-        expect(matchSpy).toBeCalledTimes(1);
+        expect(matchSpy).toBeCalledTimes(2);
     });
 
     it('should call listener', () => {
@@ -52,9 +52,10 @@ describe('Testing PrefixCommandModuleManager', () => {
     it('should create module', () => {
         const manager = createManager();
 
-        expect(createModuleSpy).toHaveBeenCalledTimes(1);
-        expect(manager.modules.length).toBe(1);
+        expect(createModuleSpy).toHaveBeenCalledTimes(2);
+        expect(manager.modules.length).toBe(2);
         expect(manager.modules[0]).toBeInstanceOf(PrefixCommandModule);
+        expect(manager.modules[1]).toBeInstanceOf(PrefixCommandModule);
     });
 
     it('should return prefix command data', () => {
