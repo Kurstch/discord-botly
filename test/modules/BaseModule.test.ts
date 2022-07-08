8ab8d76ba971a6063a6b48a19de2d8f20ed27cb4
Kurstch <61703543+Kurstch@users.noreply.github.com>
2022-07-08 15:02:22 +0300

    chore: Create tests for catch modules

--- test/modules/BaseModule.test.ts
+++ test/modules/BaseModule.test.ts
@@ -1,7 +1,9 @@
-import { Message } from 'discord.js';
+import { Collection, Message } from 'discord.js';
 import BaseModule from '../../src/modules/BaseModule';
 import dummyManager from '../mock/dummyManager';
 import type { BotlyModule, FuncParams, ModuleTypes } from '../../typings';
+import type BaseManager from '../../src/moduleManagers/BaseManager';
+import type PrefixCommandModule from '../../src/modules/PrefixCommandModule';
 
 // Because BaseModule is and abstract class, we can't test it directly.
 // Instead, we'll create a subclass and test that.
@@ -156,6 +158,36 @@ describe('Testing BaseModule', () => {
 
             await testModule.listener({} as any, ['']);
             expect(callback).toHaveBeenCalled();
+        });
+
+        describe('testing error catching', () => {
+            const catcher = jest.fn();
+            const error = new Error();
+            let testModule: DummyBaseModule<Message>;
+            const call = async () => await testModule.listener({} as Message, []);
+
+            beforeEach(() => {
+                jest.clearAllMocks();
+                const manager = { filters: new Collection(), catchers: new Collection() } as BaseManager<Message, PrefixCommandModule>;
+                testModule = new DummyBaseModule(manager, 'root/test.js', {
+                    execute: () => { throw error; }
+                });
+            });
+
+            it('should catch error', async () => {
+                testModule.manager.catchers.set('root/__catch.js', catcher);
+                await call();
+                expect(catcher).toBeCalledWith(error, {}, []);
+            });
+
+            it('should not catch error when there is a catcher', async () => {
+                testModule.manager.catchers.set('root/sub-dir/__catch.js', catcher);
+                await expect(call()).rejects.toThrowError(error);
+            });
+
+            it('should not catch error when there is not a catcher', async () => {
+                await expect(call()).rejects.toThrowError(error);
+            });
         });
     });
 });
